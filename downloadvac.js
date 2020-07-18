3ef679b9c09ebd04ef9a8e252b83c2167f5e8d33
Jordan Taylor <nadrojtayl@hotmail.com>
2020-07-18 12:41:53 -0400

    set text color

--- downloadvac.js
+++ downloadvac.js
@@ -386,7 +386,7 @@ function exportElemToExpo(name,int, page, childrenAdditionalStyle, clickfunction
             alignItems: 'center',
             flexDirection: 'row',
             position:'absolute',top:0,left:0, backgroundColor:'#8fd158', alignItems:'center',justifyContent:'center', height: "7%",  title:'Test', borderColor: 'gray', color:'black', borderRadius:15, borderWidth: 1},`+ JSON.stringify(childrenAdditionalStyle) +`]}
-        ><Text>{`+ (childrenAdditionalStyle.innerText !== undefined ? childrenAdditionalStyle.innerText:"")
+        ><Text style = {{color:`+ childrenAdditionalStyle.color ? childrenAdditionalStyle.color:"black" +`}}>{`+ (childrenAdditionalStyle.innerText !== undefined ? childrenAdditionalStyle.innerText:"")
         +  `}</Text>
         </TouchableOpacity>`
 
